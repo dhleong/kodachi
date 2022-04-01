f988dbac64287e4c11f9c30e18e7322423982a5c
Daniel Leong <me@dhleong.net>
2022-03-31 23:50:58 -0400

    Begin scaffolding an stdio-based daemon channel

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
-use std::{env, io};
+use std::{
+    env,
+    io::{self, BufRead, Read, Stdin},
+};
 
 mod connection;
+mod daemon;
 mod transport;
+
+struct StdinReader(Stdin);
+
+impl Read for StdinReader {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        self.0.lock().read(buf)
+    }
+}
+
+impl BufRead for StdinReader {
+    fn read_line(&mut self, buf: &mut String) -> io::Result<usize> {
+        self.0.lock().read_line(buf)
+    }
+
+    fn fill_buf(&mut self) -> io::Result<&[u8]> {
+        panic!("Not supported");
+    }
+
+    fn consume(&mut self, amt: usize) {
+        self.0.lock().consume(amt)
+    }
+}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> io::Result<()> {
@@ -14,7 +40,8 @@ async fn main() -> io::Result<()> {
         ))
         .await??;
     } else {
-        println!("TODO: daemon mode");
+        let stdin = StdinReader(io::stdin());
+        tokio::spawn(daemon::daemon(stdin)).await??;
     }
     Ok(())
 }
