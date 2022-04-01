6fa120a3fc4ea962b9cb886a4e31b2fcb71ffc25
Daniel Leong <me@dhleong.net>
2022-04-01 11:04:30 -0400

    Improve the Channel abstraction for ergonomics and async use

--- src/daemon/channel.rs
+++ src/daemon/channel.rs
@@ -1,22 +1,56 @@
-use std::io::Write;
+use std::{
+    io::Write,
+    sync::{Arc, Mutex},
+};
 
 use super::{protocol::Response, responses::DaemonResponse};
 
-pub struct Channel<'a, TWrite> {
-    request_id: u64,
-    write: &'a mut TWrite,
-}
+struct LockedWriter(Arc<Mutex<Box<dyn Write>>>);
 
-impl<'a, TWrite: Write> Channel<'a, TWrite> {
-    pub fn new(request_id: u64, write: &'a mut TWrite) -> Self {
-        Self { request_id, write }
+impl Write for LockedWriter {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.0.lock().unwrap().write(buf)
     }
 
-    pub fn respond(self, payload: DaemonResponse) {
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.0.lock().unwrap().flush()
+    }
+}
+
+pub struct Channel {
+    request_id: u64,
+    writer: LockedWriter,
+}
+
+impl Channel {
+    pub fn respond(&mut self, payload: DaemonResponse) {
         let response = Response {
             request_id: self.request_id,
             payload,
         };
-        serde_json::to_writer(self.write, &response).expect("Failed to write response");
+
+        serde_json::to_writer(&mut self.writer, &response).expect("Failed to write response");
+        self.writer.write_all(b"\n").unwrap();
+    }
+}
+
+pub struct ChannelSource {
+    writer: Arc<Mutex<Box<dyn Write>>>,
+}
+
+impl ChannelSource {
+    pub fn new(writer: Box<dyn Write>) -> Self {
+        Self {
+            writer: Arc::new(Mutex::new(writer)),
+        }
+    }
+}
+
+impl ChannelSource {
+    pub fn create_with_request_id(&self, request_id: u64) -> Channel {
+        Channel {
+            request_id,
+            writer: LockedWriter(self.writer.clone()),
+        }
     }
 }
--- src/daemon/mod.rs
+++ src/daemon/mod.rs
@@ -9,31 +9,34 @@ use commands::DaemonCommand;
 
 use crate::daemon::responses::DaemonResponse;
 
-use self::{channel::Channel, protocol::Request};
+use self::{
+    channel::{Channel, ChannelSource},
+    protocol::Request,
+};
 
 struct Daemon {}
 
 impl Daemon {
-    fn connect<'a, W: Write>(&self, channel: Channel<'a, W>, data: commands::Connect) {
+    fn connect(&self, mut channel: Channel, data: commands::Connect) {
         println!("TODO: connect @ {}", data.uri);
         channel.respond(DaemonResponse::Connected { id: 42 })
     }
 }
 
-pub async fn daemon<TInput: BufRead, TResponse: Write>(
+pub async fn daemon<TInput: BufRead, TResponse: 'static + Write>(
     input: TInput,
-    mut response: TResponse,
+    response: TResponse,
 ) -> io::Result<()> {
     let daemon = Daemon {};
+    let channels = ChannelSource::new(Box::new(response));
 
     for read in input.lines() {
         let raw_json = read?;
         let request: Request = serde_json::from_str(&raw_json).unwrap();
-        let channel = Channel::new(request.id, &mut response);
+        let channel = channels.create_with_request_id(request.id);
 
         match request.payload {
             DaemonCommand::Quit => break,
-
             DaemonCommand::Connect(data) => daemon.connect(channel, data),
         }
     }
