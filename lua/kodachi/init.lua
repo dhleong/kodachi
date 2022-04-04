92250336569c887e118d2dcc111c433aba6de787
Daniel Leong <me@dhleong.net>
2022-04-03 23:03:31 -0400

    Refactor connection state handling to not live as a buffer variable
    Much easier to manipulate

--- lua/kodachi/init.lua
+++ lua/kodachi/init.lua
@@ -1,3 +1,5 @@
+local states = require'kodachi.states'
+
 local local_path = debug.getinfo(1, 'S').source:sub(2)
 local kodachi_root = vim.fn.fnamemodify(local_path, ':h:h:h')
 local kodachi_tmux = kodachi_root .. '/config/tmux.conf'
@@ -12,8 +14,9 @@ local M = {
 
 ---@param uri string
 function M.buf_connect(uri)
-  if vim.b.kodachi then
-    if not vim.b.kodachi.exited then
+  local existing = states.current { silent = true }
+  if existing then
+    if not existing.exited then
       -- TODO: Acutally, the *daemon* is live, but we could be disconnected
       print('kodachi: A connection is already live in this buffer')
       return
@@ -28,9 +31,8 @@ function M.buf_connect(uri)
   local tmux_wrap = vim.fn.has('nvim')
 
   local socket = require'kodachi.socket'.create()
-  M.sockets[socket.name] = socket
+  local state = states.create_for_buf { socket = socket }
 
-  local state = { socket = socket.name }
   local cmd = vim.tbl_flatten {
     tmux_wrap and { 'tmux', '-f', kodachi_tmux, 'new-session', '-n', 'kodachi' } or {},
     M.debug and { 'cargo', 'run', '--' } or kodachi_exe,
@@ -41,7 +43,6 @@ function M.buf_connect(uri)
     cwd = kodachi_root,
     on_exit = function (_, _, _)
       state.exited = true
-      vim.b[state.bufnr].kodachi = state
 
       -- Clean up after ourselves
       vim.fn.delete(socket.name)
@@ -50,12 +51,10 @@ function M.buf_connect(uri)
 
   state.job_id = job_id
   state.bufnr = vim.fn.bufnr('%')
-  vim.b.kodachi = state
 
   local request_id = M.buf_request { type = 'Connect', uri = uri }
   socket:await_request_id(request_id, function (response)
     state.connection_id = response.id
-    vim.b.kodachi = state
   end)
 
   return job_id
@@ -63,34 +62,27 @@ end
 
 ---@param request KodachiRequest
 function M.buf_request(request)
-  if not vim.b.kodachi then
+  local state = states.current()
+  if not state then
     return
   end
 
-  local socket = M.sockets[vim.b.kodachi.socket]
-  request.id = socket:next_request_id()
+  request.id = state.socket:next_request_id()
 
   local to_write = vim.fn.json_encode(request) .. '\n'
 
-  socket:write(to_write)
+  state.socket:write(to_write)
 
   return request.id
 end
 
 function M.buf_send(text)
-  -- TODO: Get the connection ID from the buffer
-  if not vim.b.kodachi then
-    print('Not attached to any kodachi instance.')
+  local state = states.current_connected()
+  if not state then
     return
   end
 
-  local connection = vim.b.kodachi.connection_id
-  if not connection then
-    print('Not connected.')
-    return
-  end
-
-  M.buf_request { type = "Send", connection = connection, text = text }
+  M.buf_request { type = "Send", connection = state.connection_id, text = text }
 end
 
 return M
---
+++ lua/kodachi/states.lua
@@ -0,0 +1,49 @@
+---@alias KodachiState { exited:boolean|nil }
+
+local M = {
+  states = {},
+}
+
+---@param initial_state KodachiState
+---@return KodachiState
+function M.create_for_buf(initial_state)
+  local state = initial_state or {}
+  rawset(M.states, vim.fn.bufnr('%'), state)
+  return state
+end
+
+---@param opts { silent:boolean }|nil
+function M.current(opts)
+  local state = M[vim.fn.bufnr('%')]
+
+  if not state and (not opts or not opts.silent) then
+    print('Not connected to a kodachi session')
+  end
+
+  return state
+end
+
+function M.current_connected()
+  local state = M.current()
+
+  if not state or not state.connection_id then
+    print('Not connected.')
+    return
+  end
+
+  return state
+end
+
+setmetatable(M, {
+  __index = function (_, bufnr)
+    return M.states[bufnr]
+  end,
+
+  __newindex = function (_, bufnr, state)
+    if M.states[bufnr] or M.states[state.bufnr] then
+      M.states[bufnr] = state
+    end
+  end,
+})
+
+return M
