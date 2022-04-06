1bf9d58c9e3b7ceff8966e490cd064621536523e
Daniel Leong <me@dhleong.net>
2022-04-05 21:32:41 -0400

    Restore `debug` setting for the term UI

--- lua/kodachi/ui/term.lua
+++ lua/kodachi/ui/term.lua
@@ -3,20 +3,22 @@ local kodachi_root = vim.fn.fnamemodify(local_path, ':h:h:h')
 local kodachi_tmux = kodachi_root .. '/config/tmux.conf'
 local kodachi_exe = kodachi_root .. '/target/release/kodachi'
 
-local M = {}
+local M = {
+  debug = true,
+}
 
 ---@param opts {socket_name:string, on_exit:any}
 function M.spawn_unix(opts)
-  local socket_name = opts.socket_name
-
   -- NOTE: Neovim does not correctly persist output if the window resizes smaller
   -- than the width of the text, so we use tmux to save it
   local tmux_wrap = vim.fn.has('nvim')
 
+  -- TODO If not debug, ensure the release executable is compiled/up-to-date
+
   local cmd = vim.tbl_flatten {
     tmux_wrap and { 'tmux', '-f', kodachi_tmux, 'new-session', '-n', 'kodachi' } or {},
     M.debug and { 'cargo', 'run', '--' } or kodachi_exe,
-    'unix', socket_name,
+    'unix', opts.socket_name,
   }
 
   local job_id = vim.fn.termopen(cmd, {
