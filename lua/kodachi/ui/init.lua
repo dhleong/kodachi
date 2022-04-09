56153c6ae2a525a54b11aef32f4675d5805d2391
Daniel Leong <me@dhleong.net>
2022-04-08 21:05:21 -0400

    Associate state with the buffer from which a connection was initiated
    This *may* not be the right way to handle this, but it's convenient when
    sourcing a script file to initiate a connection

--- lua/kodachi/ui/init.lua
+++ lua/kodachi/ui/init.lua
@@ -5,6 +5,7 @@ local M = {}
 ---Ensures that the current window is a valid kodachi output window
 ---@return KodachiState associated with the buffer
 function M.ensure_window()
+  local initial_bufnr = vim.fn.bufnr('%')
   local existing = states.current { silent = true }
   if existing then
     if not existing.exited and existing.connection_id then
@@ -14,7 +15,7 @@ function M.ensure_window()
 
     -- Reuse the window with a new buffer
     vim.cmd [[ enew ]]
-  elseif vim.bo.modified or vim.fn.bufname('%') ~= '' then
+  elseif vim.bo.modified or vim.fn.bufname('%') ~= '' or initial_bufnr == existing.initial_bufnr then
     -- No existing state in this buffer, and the buffer is modified or associated
     -- with a file on disk; go ahead and open a split
     vim.cmd [[ vsplit | enew ]]
@@ -23,10 +24,16 @@ function M.ensure_window()
   local socket = require'kodachi.socket'.create()
   local state = states.create_for_buf { socket = socket }
 
+  -- Share the state with the source buffer, I guess? This facilitates reloading the
+  -- script for a live connection to update mappings, etc.
+  states[initial_bufnr] = state
+  state.initial_bufnr = initial_bufnr
+
   local job_id = require'kodachi.ui.term'.spawn_unix {
     socket_name = socket.name,
     on_exit = function ()
       state.exited = true
+      state.connection_id = nil
 
       -- Clean up after ourselves
       vim.fn.delete(socket.name)
