eb208f3ec422ca2ac138ce4310337aae71be756d
Daniel Leong <me@dhleong.net>
2022-05-02 09:28:38 -0400

    Prepare logic and utilities for handling Triggers

---
+++ src/app/processing/ansi.rs
@@ -0,0 +1,42 @@
+use std::ops::Deref;
+
+use bytes::{BufMut, BytesMut};
+
+#[derive(Clone, Default)]
+pub struct Ansi(BytesMut);
+
+impl Deref for Ansi {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        // TODO Strip ANSI codes
+        ""
+    }
+}
+
+impl AsRef<[u8]> for Ansi {
+    #[inline]
+    fn as_ref(&self) -> &[u8] {
+        self.0.as_ref()
+    }
+}
+
+impl Into<BytesMut> for Ansi {
+    fn into(self) -> BytesMut {
+        self.0
+    }
+}
+
+impl Ansi {
+    pub fn put_slice(&mut self, bytes: &[u8]) {
+        self.0.put_slice(bytes)
+    }
+
+    pub fn take(&mut self) -> BytesMut {
+        let result = self.0.clone();
+
+        self.0.clear();
+
+        return result;
+    }
+}
--- src/app/processing/text.rs
+++ src/app/processing/text.rs
@@ -1,9 +1,12 @@
 use bytes::BytesMut;
+use regex::Regex;
 
 use crate::{
     app::{matchers::MatcherSpec, Id},
     daemon::channel::RespondedChannel,
 };
+
+use super::ansi::Ansi;
 
 struct RegisteredMatcher {
     matcher: MatcherSpec, // TODO compiled Matcher
@@ -13,6 +16,7 @@ struct RegisteredMatcher {
 #[derive(Default)]
 pub struct TextProcessor {
     matchers: Vec<RegisteredMatcher>,
+    pending_line: Ansi,
 }
 
 impl TextProcessor {
@@ -22,9 +26,21 @@ impl TextProcessor {
         connection_id: Id,
         notifier: &mut RespondedChannel,
     ) -> BytesMut {
+        // TODO: Read up until a newline from text; push that onto pending_line
+        // TODO: If we *don't* have a full line (and, if we don't already have a SavePosition set, IE from
+        // a previous partial line, emit SavePosition) emit the pending
+        // TODO: If we *do* have a full line in pending_Line, pop it off and feed it to matchers;
+        // if none "consume" the input, emit. If *any* consume, and we have a SavePosition set,
+        // emit RestorePosition + Clear(ClearType::UntilNewLine)
+        self.pending_line.put_slice(&text);
+        Regex::new("foo")
+            .unwrap()
+            .find(&self.pending_line)
+            .expect("hi");
+
         // TODO It might be better if we could avoid a dependency on RespondedChannel here,
         // and emit results rather than sending them directly...
-        text
+        self.pending_line.take()
     }
 
     // TODO compiled Matcher
