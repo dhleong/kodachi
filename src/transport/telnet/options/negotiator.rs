27e7019da82b2d4817e60d8e5b233b460211b0dc
Daniel Leong <me@dhleong.net>
2022-12-13 21:34:14 -0500

    Fix handling of Dont negotiations after a Do

--- src/transport/telnet/options/negotiator.rs
+++ src/transport/telnet/options/negotiator.rs
@@ -46,14 +46,29 @@ impl OptionsNegotiator {
         }
 
         let response_type = match negotiation {
-            NegotiationType::Do => NegotiationType::Wont,
-            NegotiationType::Will => NegotiationType::Dont,
-            _ => panic!("Impossible negotiation {:?} for {:?}", negotiation, option),
+            NegotiationType::Do => Some(NegotiationType::Wont),
+            NegotiationType::Will => Some(NegotiationType::Dont),
+            NegotiationType::Dont => {
+                if self.options.get(&option) == Some(&OptionState::Do) {
+                    self.options
+                        .insert(option, OptionState::Accept(NegotiationType::Do));
+                }
+                Some(NegotiationType::Wont)
+            }
+            NegotiationType::Wont => {
+                if self.options.get(&option) == Some(&OptionState::Will) {
+                    self.options
+                        .insert(option, OptionState::Accept(NegotiationType::Will));
+                }
+                None
+            }
         };
 
-        let response = TelnetEvent::Negotiate(response_type, option);
-        trace!(target: "telnet", ">> {:?}", response);
-        response.write_all(stream).await?;
+        if let Some(response_type) = response_type {
+            let response = TelnetEvent::Negotiate(response_type, option);
+            trace!(target: "telnet", ">> {:?}", response);
+            response.write_all(stream).await?;
+        }
 
         Ok(())
     }
@@ -82,5 +97,91 @@ impl OptionsNegotiatorBuilder {
         self.options
             .insert(option, OptionState::Accept(NegotiationType::Will));
         self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::task::Poll;
+
+    use bytes::BytesMut;
+
+    use super::*;
+
+    struct TestStream {
+        sent: BytesMut,
+    }
+
+    impl TestStream {
+        pub fn new() -> Self {
+            Self {
+                sent: BytesMut::default(),
+            }
+        }
+    }
+
+    impl AsyncWrite for TestStream {
+        fn poll_write(
+            mut self: std::pin::Pin<&mut Self>,
+            _cx: &mut std::task::Context<'_>,
+            buf: &[u8],
+        ) -> std::task::Poll<Result<usize, io::Error>> {
+            self.sent.extend_from_slice(buf);
+            Poll::Ready(Ok(buf.len()))
+        }
+
+        fn poll_flush(
+            self: std::pin::Pin<&mut Self>,
+            _cx: &mut std::task::Context<'_>,
+        ) -> std::task::Poll<Result<(), io::Error>> {
+            todo!()
+        }
+
+        fn poll_shutdown(
+            self: std::pin::Pin<&mut Self>,
+            _cx: &mut std::task::Context<'_>,
+        ) -> std::task::Poll<Result<(), io::Error>> {
+            todo!()
+        }
+    }
+
+    #[tokio::test]
+    async fn dont_after_do_test() -> io::Result<()> {
+        let mut handler = OptionsNegotiatorBuilder::default()
+            .accept_do(TelnetOption::TermType)
+            .build();
+
+        let mut stream = TestStream::new();
+
+        handler
+            .negotiate(NegotiationType::Do, TelnetOption::TermType, &mut stream)
+            .await?;
+
+        handler
+            .negotiate(NegotiationType::Dont, TelnetOption::TermType, &mut stream)
+            .await?;
+
+        let mut expected_stream = TestStream::new();
+        TelnetEvent::Negotiate(NegotiationType::Will, TelnetOption::TermType)
+            .write_all(&mut expected_stream)
+            .await?;
+        TelnetEvent::Negotiate(NegotiationType::Wont, TelnetOption::TermType)
+            .write_all(&mut expected_stream)
+            .await?;
+        assert_eq!(stream.sent, expected_stream.sent);
+
+        // If the server changes their mind, our state should be ready for that
+        expected_stream.sent.clear();
+        stream.sent.clear();
+        handler
+            .negotiate(NegotiationType::Do, TelnetOption::TermType, &mut stream)
+            .await?;
+
+        TelnetEvent::Negotiate(NegotiationType::Will, TelnetOption::TermType)
+            .write_all(&mut expected_stream)
+            .await?;
+        assert_eq!(stream.sent, expected_stream.sent);
+
+        Ok(())
     }
 }
